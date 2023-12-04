1b1da937d71746575148eb3a9d14c0536fd7b320
ebochkov <ebochkov@itkey.com>
2023-12-04 20:14:50 +0300

    deploy_crt_on_nodes bug fix

--- deploy_crts_on_nodes.sh
+++ deploy_crts_on_nodes.sh
@@ -26,33 +26,38 @@ do
         -ih, 	-install_home		<install_home_dir>
         -s, 	-settings		    <path_settings_file>
         -l,   -by_list        <deploy_by_ips_list_bool>
-        -gk,	-gitlab_key		  <deploy_gitlab_key_bool>
-        -lk,	-lcm_key	  	  <deploy_lcm_key_bool>
-        -dc,	-docker_cfg		  <deploy_docker_cfg_bool>
-        -nc,	-nexus_crt		  <deploy_nexus_crt_bool>
+        -cc,  -ca_crt         <deploy_ca_crt_bool>
         "
+#        -gk,	-gitlab_key		  <deploy_gitlab_key_bool>
+#        -lk,	-lcm_key	  	  <deploy_lcm_key_bool>
+#        -dc,	-docker_cfg		  <deploy_docker_cfg_bool>
+#        -nc,	-nexus_crt		  <deploy_nexus_crt_bool>
+#        "
             exit 0
             break ;;
 	      -ih|-install_home) INSTALL_HOME="$2"
-	          echo "Found the -t <install_home_dir> option, with parameter value $INSTALL_HOME"
+	          echo "Found the -install_home <install_home_dir> option, with parameter value $INSTALL_HOME"
             shift ;;
         -s|-settings) SETTINGS="$2"
-	          echo "Found the -t <path_settings_file> option, with parameter value $SETTINGS"
+	          echo "Found the -settings <path_settings_file> option, with parameter value $SETTINGS"
             shift ;;
         -l|-by_list) DEPLOY_BY_IPS_LIST="$2"
-	          echo "Found the -t <deploy_by_ips_list_bool> option, with parameter value $DEPLOY_BY_IPS_LIST"
+	          echo "Found the -by_list <deploy_by_ips_list_bool> option, with parameter value $DEPLOY_BY_IPS_LIST"
             shift ;;
        -gk|-gitlab_key) DEPLOY_GITLAB_KEY="$2"
-	          echo "Found the -t <deploy_gitlab_key_bool> option, with parameter value $DEPLOY_GITLAB_KEY"
+	          echo "Found the -gitlab_key <deploy_gitlab_key_bool> option, with parameter value $DEPLOY_GITLAB_KEY"
             shift ;;
         -lk|-lcm_key) DEPLOY_LCM_KEY="$2"
-	          echo "Found the -t <deploy_lcm_key_bool> option, with parameter value $DEPLOY_LCM_KEY"
+	          echo "Found the -lcm_key <deploy_lcm_key_bool> option, with parameter value $DEPLOY_LCM_KEY"
             shift ;;
         -dc|-docker_cfg) DEPLOY_DOCKER_CFG="$2"
-	          echo "Found the -t <deploy_docker_cfg_bool> option, with parameter value $DEPLOY_DOCKER_CFG"
+	          echo "Found the -docker_cfg <deploy_docker_cfg_bool> option, with parameter value $DEPLOY_DOCKER_CFG"
             shift ;;
         -nc|-nexus_crt) DEPLOY_NEXUS_CRTS="$2"
-	          echo "Found the -t <deploy_nexus_crt_bool> option, with parameter value $DEPLOY_NEXUS_CRTS"
+	          echo "Found the -nexus_crt <deploy_nexus_crt_bool> option, with parameter value $DEPLOY_NEXUS_CRTS"
+            shift ;;
+        -cc|-ca_crt) DEPLOY_CA_CRTS="$2"
+            echo "Found the -ca_crt <deploy_ca_crt_bool> option, with parameter value $DEPLOY_CA_CRTS"
             shift ;;
         --) shift
             break ;;
@@ -70,27 +75,31 @@ deploy_and_copy () {
     fi
     #for IP in "${IPS[@]}"; do
     for IP in "${IPS_ARRAY[@]}"; do
-      if [ "$DEPLOY_LCM_KEY" = true ] ; then
-        echo "Copy public key from lcm to ${IP}"
-        ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub $IP
-      fi
-      if [ "$DEPLOY_GITLAB_KEY" = true ] ; then
-        echo "Copy gitlab key to ${IP}"
-        KEY=$(cat $INSTALL_HOME/config/gitlab_key.pub)
-        ssh -o StrictHostKeyChecking=no $IP echo '"$KEY"' >> ~/.ssh/authorized_keys
-      fi
-      if [ "$DEPLOY_NEXUS_CRTS" = true ] ; then
-        echo "Deploy nexus crt to $IP"
-        ssh -o StrictHostKeyChecking=no $IP mkdir -p /etc/docker/certs.d/nexus.$DOMAIN:5000
-        scp -o StrictHostKeyChecking=no $INSTALL_HOME/data/ca/installer/certs/nexus.crt $IP:/etc/docker/certs.d/nexus.$DOMAIN:5000/nexus.crt
-        ssh -o StrictHostKeyChecking=no $IP chmod 444 /etc/docker/certs.d/nexus.$DOMAIN:5000/nexus.crt
-      fi
-      if [ "$DEPLOY_DOCKER_CFG" = true ] ; then
-        echo "Deploy docker cfg to $IP"
-        ssh -o StrictHostKeyChecking=no $IP mkdir -p ~/.docker
-        scp -o StrictHostKeyChecking=no $INSTALL_HOME/config/docker_auth.json $IP:~/.docker/config.json
-        ssh -o StrictHostKeyChecking=no $IP chmod 600 ~/.docker/config.json
-      fi
+#      if [ "$DEPLOY_LCM_KEY" = true ] ; then
+#        echo "Copy public key from lcm to ${IP}"
+#        ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub $IP
+#      fi
+#      if [ "$DEPLOY_GITLAB_KEY" = true ] ; then
+#        echo "Copy gitlab key to ${IP}"
+#        KEY=$(cat $INSTALL_HOME/config/gitlab_key.pub)
+#        ssh -o StrictHostKeyChecking=no $IP echo '"$KEY"' >> ~/.ssh/authorized_keys
+#      fi
+#      if [ "$DEPLOY_NEXUS_CRTS" = true ] ; then
+#        echo "Deploy nexus crt to $IP"
+#        ssh -o StrictHostKeyChecking=no $IP mkdir -p /etc/docker/certs.d/nexus.$DOMAIN:5000
+#        scp -o StrictHostKeyChecking=no $INSTALL_HOME/data/ca/installer/certs/nexus.crt $IP:/etc/docker/certs.d/nexus.$DOMAIN:5000/nexus.crt
+#        ssh -o StrictHostKeyChecking=no $IP chmod 444 /etc/docker/certs.d/nexus.$DOMAIN:5000/nexus.crt
+#      fi
+#      if [ "$DEPLOY_DOCKER_CFG" = true ] ; then
+#        echo "Deploy docker cfg to $IP"
+#        ssh -o StrictHostKeyChecking=no $IP mkdir -p ~/.docker
+#        scp -o StrictHostKeyChecking=no $INSTALL_HOME/config/docker_auth.json $IP:~/.docker/config.json
+#        ssh -o StrictHostKeyChecking=no $IP chmod 600 ~/.docker/config.json
+#      fi
+       if [ "$CA_CRT" = true ] ; then
+           scp -o StrictHostKeyChecking=no $INSTALL_HOME/data/ca/root/ca.crt $IP:/usr/local/share/ca-certificates/ca.crt
+           ssh -o StrictHostKeyChecking=no $IP update-ca-certificates
+       fi
     done
 }
 
