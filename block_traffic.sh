ba3a44582bd515fe272230a4a8511e01228af4f2
ebochkov <ebochkov@itkey.com>
2023-12-07 12:24:29 +0300

    check nova consul bug fix

--- block_traffic.sh
+++ block_traffic.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-# test push
-# IPS controls list
-#BLOCKED_IPS=("10.224.51.11" "10.224.51.12" "10.224.51.13" "10.224.51.2" "10.224.51.4")
+
+# The script blocks all traffic to and from the IP list nodes.
+# The script must be copied to the desired compute node and run using the ssh command from another node.
+
+TIMEOUT=180
+
+# IPS nodes list
+#example: BLOCKED_IPS=("<IP_ctrl_1>" "<IP_ctrl_2>" "<IP_3>" "...")
 BLOCKED_IPS=("10.224.133.138" "10.224.133.139" "10.224.133.133" "10.224.133.134" "10.224.133.135")
 
 block_traffic () {
     for IP in "${BLOCKED_IPS[@]}"; do
 # Blocking incoming traffic from IP
         echo "Block incoming traffic from ${IP}"
-        iptables -A INPUT -s $IP -j DROP
+        iptables -A INPUT -s "$IP" -j DROP
 #Blocking outgoing traffic to IP
         echo "Block outgoing traffic to ${IP}"
-        iptables -A OUTPUT -d $IP -j DROP
+        iptables -A OUTPUT -d "$IP" -j DROP
         date
     done
 }
@@ -19,17 +24,17 @@ block_traffic () {
 enable_traffic () {
     for IP in "${BLOCKED_IPS[@]}"; do
 # Deleting incoming traffic rule from IP
-        echo "Deleting incoming traffic rule from IP"
-        iptables -D INPUT -s ${IP} -j DROP
+        echo "Deleting incoming traffic rule from $IP"
+        iptables -D INPUT -s "$IP" -j DROP
 # Deleting outgoing traffic rule from IP
-        echo "Deleting outgoing traffic rule from IP"
-        iptables -D OUTPUT -d ${IP} -j DROP
+        echo "Deleting outgoing traffic rule from $IP"
+        iptables -D OUTPUT -d "$IP" -j DROP
         date
     done
 }
 
 block_traffic
-echo "The server is isolated from ${BLOCKED_IPS[@]}"
+echo "The server is isolated from: ${BLOCKED_IPS[*]}"
 iptables -S
-#sleep 180
-#enable_traffic
+sleep $TIMEOUT
+enable_traffic
