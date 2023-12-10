aea8c1d1f7546ee604868eb2fb390a7dde3a8ab6
ebochkov <ebochkov@itkey.com>
2023-12-11 02:26:37 +0300

    check nova consul bug fix

--- check_consul_log.sh
+++ check_consul_log.sh
@@ -53,7 +53,7 @@ do
         -e 's/\(.*IPMI "power off".*\)/\o033[31m\1\o033[39m/' \
         -e 's/\(.*disabled,.*\)/\o033[33m\1\o033[39m/'
     ssh -o StrictHostKeyChecking=no "$NODE_NAME" 'echo -e "\033[0;35m$(date)\033[0m
-\033[0;35m$(hostname)\033[0m
+\033[0;35mLogs from: $(hostname)\033[0m
 \033[0;35mFor check this log: \"ssh $(hostname) less /var/log/kolla/autoevacuate.log | less\"\033[0m"'
 
     sleep "$OUTPUT_PERIOD"
