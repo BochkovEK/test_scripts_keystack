1e63f22dabe3689006f897e0d51a2ec21cbc7a30
ebochkov <ebochkov@itkey.com>
2023-12-11 11:22:26 +0300

    check nova consul bug fix

--- block_traffic.sh
+++ block_traffic.sh
@@ -4,7 +4,7 @@
 # The script must be copied to the desired compute node and run using the ssh command from another node.
 # For start this script, need create file ~/blocked_ips_list :
 
-
+echo 'Start block_traffic.sh script'
 if [ -f ~/blocked_ips_list ]; then BLOCKED_IPS=$(cat ~/blocked_ips_list); else echo "IPS list to block not found (~/blocked_ips_list)"; exit 1; fi
 #[[ -z $BLOCKED_IPS ]] && { echo "IPS list to block not found (env BLOCKED_IPS)"; exit 1; }
 
