40afd00a5529809cbb4de20036be25245beea36f
ebochkov <ebochkov@itkey.com>
2023-12-25 17:44:49 +0300

    bug fix

--- check_before_installer.sh
+++ check_before_installer.sh
@@ -159,7 +159,7 @@ compare_version () {
 
 check_rpm_packages () {
 
-    for package in ${required_rpm[@]}; do
+    for package in "${required_rpm[@]}"; do
 #        echo package $package
         package_name=$(echo $package | awk -F ":" '{print $1}')
 #        echo package_name $package_name
@@ -176,7 +176,7 @@ check_rpm_packages () {
 
 check_dpkg_packages () {
     packages=("$@")
-    for package in ${packages[@]}; do
+    for package in "${packages[@]}"; do
 #        echo package $package
         package_name=$(echo $package | awk -F ":" '{print $1}')
 #        echo package_name: $package_name
@@ -250,7 +250,9 @@ check_docker_status () {
 check_docker_compose_file () {
 
 #    source $SETTINGS_FILE
-    check_comp_file=$(${docker_compose_command} -f $COMPOSE_FILE config)
+    #check_comp_file=$(
+    ${docker_compose_command} -f $COMPOSE_FILE config
+    #)
 
     if [ $? -eq 0 ]; then
         print_ok "Docker compose file ($COMPOSE_FILE)"
@@ -280,7 +282,7 @@ clear
 echo -e "\n====== Insatlled packages checking ======\n"
 case $os in
     ubuntu)
-        check_dpkg_packages ${required_rpm[@]}
+        check_dpkg_packages "${required_rpm[@]}"
     ;;
     *)
         check_rpm_packages
