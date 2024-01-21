32ea011eca6f96fe58c5a990eac9fe8278a54d68
ebochkov <ebochkov@itkey.com>
2024-01-21 21:36:10 +0300

    bug fix

--- check_drs_logs.sh
+++ check_drs_logs.sh
@@ -12,5 +12,6 @@ srv=$(cat /etc/hosts | grep -E "$CTRL_NODES" | awk '{print $1}')
 for host in $srv;do
     echo -e "${CYAN}Drs logs on $(cat /etc/hosts | grep -E ${host} | awk '{print $2}'):${NC}"
     ssh -o StrictHostKeyChecking=no $host tail -${TAIL_NUM} /var/log/kolla/drs/drs.log; echo -e "${BLUE}`date`${NC}"
-    echo -e "${ORANGE}For read all log on $host: ssh -t -o StrictHostKeyChecking=no $host less /var/log/kolla/drs/drs.log${NC}"
+    echo -e "For read all log on $host:"
+    echo -e "${ORANGE}ssh -t -o StrictHostKeyChecking=no $host less /var/log/kolla/drs/drs.log${NC}"
 done
