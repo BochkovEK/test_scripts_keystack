76978513ad0063303e36ab28ab6ee4ed4102ab8b
ebochkov <ebochkov@itkey.com>
2024-01-25 20:59:35 +0300

    bug fix

--- block_traffic.sh
+++ block_traffic.sh
@@ -2,7 +2,12 @@
 
 # The script blocks all traffic to and from the IP list nodes.
 # The script must be copied to the desired compute node and run using the ssh command from another node.
-# For start this script, need create file ~/blocked_ips_list :
+# For start this script, need create file ~/blocked_ips_list
+# ~/blocked_ips_list example:
+#<IP_1>
+#<IP_2>
+#<IP_3>
+#<IP_4>
 
 echo 'Start block_traffic.sh script'
 if [ -f ~/blocked_ips_list ]; then BLOCKED_IPS=$(cat ~/blocked_ips_list); else echo "IPS list to block not found (~/blocked_ips_list)"; exit 1; fi
