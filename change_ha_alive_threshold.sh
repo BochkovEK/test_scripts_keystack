d80b679b3f02711b047c2f0f57291a9ce10d51cb
ebochkov <ebochkov@itkey.com>
2024-02-21 14:52:23 +0300

    bug fix

---
+++ change_ha_alive_threshold.sh
@@ -0,0 +1,14 @@
+# The script change alive_threshold in consul region config
+# openrc file required in ~/
+# Start scrip: bash change_ha_alive_threshold.sh <alive_threshold_value>
+
+[[ -z $OPENRC_PATH ]] && OPENRC_PATH="$HOME/openrc"
+
+[[ -z "${1}" ]] && { echo "Alive threshold value required as parameter script"; exit 1; }
+
+source $OPENRC_PATH
+
+REGION=$OS_REGION_NAME
+
+bash command_on_nodes.sh -c "sed -i 's/\"alive_compute_threshold\": \"1\"/\"alive_compute_threshold\": \"$1\"/' /etc/kolla/consul/region-config_${REGION}.json"
+bash command_on_nodes.sh -c "docker restart consul"
