8929ad7b6d249db174db3328b4b1bf0e748bc354
ebochkov <ebochkov@itkey.com>
2024-03-01 20:17:40 +0300

    bug fix

--- ha_region_config.sh
+++ ha_region_config.sh
@@ -57,7 +57,8 @@ change_alive_threshold () {
 
 change_dead_threshold () {
   alive_threshold_string=$(cat /etc/kolla/consul/region-config_${REGION}.json| grep 'alive_compute_threshold')
-  bash command_on_nodes.sh -nt ctrl -c "sed -i --regexp-extended "s/$alive_threshold_string/$alive_threshold_string\n   \"dead_compute_threshold\": \"$1\",/" /etc/kolla/consul/region-config_${REGION}.json"
+  bash command_on_nodes.sh -nt ctrl -c 'alive_threshold_string=$(cat /etc/kolla/consul/region-config_${REGION}.json| grep 'alive_compute_threshold');
+  sed -i --regexp-extended "s/$alive_threshold_string/$alive_threshold_string\n   \"dead_compute_threshold\": \"$1\",/" /etc/kolla/consul/region-config_${REGION}.json'
   conf_id_changed="true"
 }
 
