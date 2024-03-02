acb439773bc1f1db0621fa42b9cc4def56a217da
ebochkov <ebochkov@itkey.com>
2024-03-02 17:15:56 +0300

    bug fix

--- ha_region_config.sh
+++ ha_region_config.sh
@@ -103,7 +103,7 @@ change_dead_threshold () {
     sed -i --regexp-extended "s/$alive_threshold_string/${alive_threshold_string}\n   \"dead_compute_threshold\": \"$1\",/" \
     ${consul_conf_dir}/region-config_${REGION}.json
   else
-    sed -i --regexp-extended "s/"dead_compute_threshold":\s+"[0-9]+,"/\"dead_compute_threshold\": \"$1\",/" \
+    sed -i --regexp-extended "s/\"dead_compute_threshold\":\s+\"[0-9]+\",/\"dead_compute_threshold\": \"$1\",/" \
       ${consul_conf_dir}/region-config_${REGION}.json
   fi
   push_consul_conf
