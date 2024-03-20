ce24195fb3b061e9dda8911fac1bdccd4dd1b2ff
ebochkov <ebochkov@itkey.com>
2024-03-20 21:26:41 +0300

    wip test_scripts

--- installer/custom_installer.sh
+++ installer/custom_installer.sh
@@ -313,6 +313,7 @@ mkdir -p $NEXUS_HOME/{data,blobs,restore-from-backup}
 mkdir -p /etc/docker/certs.d/$NEXUS_NAME.$DOMAIN
 cp $CA_HOME/cert/chain-ca.pem /etc/docker/certs.d/$NEXUS_NAME.$DOMAIN/ca.crt
 if [[ $CLIENT_NEXUS == "y" ]]; then
+  mkdir -p /etc/docker/certs.d/$CLIENT_NEXUS_NAME
   cp $CA_HOME/cert/$CLIENT_NEXUS_NAME.pem /etc/docker/certs.d/$CLIENT_NEXUS_NAME/$CLIENT_NEXUS_NAME.crt
 fi
 #Custom_end
