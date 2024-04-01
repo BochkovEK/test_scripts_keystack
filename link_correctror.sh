d0c08f53760a837f1b767fc92d1c9d7e70f05488
ebochkov <ebochkov@itkey.com>
2024-04-01 18:39:23 +0300

    wip test_scripts

--- link_correctror.sh
+++ link_correctror.sh
@@ -1,4 +1,4 @@
-# The script for correct link from nexus web portal
+# The script for correct link from nexus web portal and upload file from link to $HOME dir
 # To get link from nexus:
 # 1) Go nexus web portal repo
 # 2) Select image
@@ -7,4 +7,5 @@
 
 [ -z $1 ] && { echo "To run the script, you need to pass a link to as an argument"; exit 1; }
 
-echo $1 | sed -e "s#%2F#/#g" | cat -
+echo $1 | sed -e "s#%2F#/#g" | tee /tmp/link_to_wget.txt
+cat /tmp/link_to_wget.txt | wget - -o $HOME/
