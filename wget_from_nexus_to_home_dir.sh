145bff6fcc48982097b5533cb815db2cdc9c6988
ebochkov <ebochkov@itkey.com>
2024-04-05 18:52:34 +0300

    wip test_scripts

--- wget_from_nexus_to_home_dir.sh
+++ wget_from_nexus_to_home_dir.sh
@@ -10,7 +10,7 @@
 echo $1 | sed -e "
   s,%2F,/,g; \
   s,#browse/browse:,repository/,; \
-  s,pip:,/," | tee /tmp/link_to_wget.txt
+  s,\:,/," | tee /tmp/link_to_wget.txt
 #wget -i /tmp/link_to_wget.txt -P $HOME/
 
 #browse/browse:
