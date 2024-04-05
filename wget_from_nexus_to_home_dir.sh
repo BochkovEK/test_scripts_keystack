39bb1f374d41f44d6b18bc661460672e94e36331
ebochkov <ebochkov@itkey.com>
2024-04-05 18:49:11 +0300

    wip test_scripts

--- wget_from_nexus_to_home_dir.sh
+++ wget_from_nexus_to_home_dir.sh
@@ -3,9 +3,13 @@
 # 1) Go nexus web portal repo
 # 2) Select image
 # 3) Summary -> Path -> RCM Copy link address
-# To start: bash link_corrector.sh <link_from_nexus_web_portal>
+# To start: bash wgetfrom_nexus_to_home_dir.sh <link_from_nexus_web_portal>
 
 [ -z $1 ] && { echo "To run the script, you need to pass a link to as an argument"; exit 1; }
 
-echo $1 | sed -e "s#%2F#/#g" | tee /tmp/link_to_wget.txt
-wget -i /tmp/link_to_wget.txt -P $HOME/
+echo $1 | sed -e "s,%2F,/,g; \
+ s,#browse/browse:,repository/,; \
+ s,:,/," | tee /tmp/link_to_wget.txt
+#wget -i /tmp/link_to_wget.txt -P $HOME/
+
+#browse/browse:
