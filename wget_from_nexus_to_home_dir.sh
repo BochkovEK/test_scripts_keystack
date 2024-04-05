6bfc46513195b5b5cae08d16d8130738cd3334bb
ebochkov <ebochkov@itkey.com>
2024-04-05 19:19:02 +0300

    wip test_scripts

--- wget_from_nexus_to_home_dir.sh
+++ wget_from_nexus_to_home_dir.sh
@@ -8,9 +8,9 @@
 [ -z $1 ] && { echo "To run the script, you need to pass a link to as an argument"; exit 1; }
 
 echo $1 | sed -e "
-  s,%2F,/," | tee #; \
-#  s,#browse/browse:,repository/,;
-#  s,:,/packages/,2" | tee /tmp/link_to_wget.txt
+  s,%2F,/g,; \
+  s,#browse/browse:,repository/,;
+  s,:,/packages/,2" | tee /tmp/link_to_wget.txt
 #wget -i /tmp/link_to_wget.txt -P $HOME/
 
 #browse/browse:
