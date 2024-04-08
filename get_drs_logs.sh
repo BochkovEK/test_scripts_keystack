638852dd12dbb14023c23593d9e175e6584cb41d
ebochkov <ebochkov@itkey.com>
2024-04-08 13:18:36 +0300

    wip test_scripts

--- get_drs_logs.sh
+++ get_drs_logs.sh
@@ -89,6 +89,7 @@ get_drs_logs () {
 #    echo "Add logs to archive... drs-logs-"`date +"%d-%m-%Y"`""
 #    archive_logs_name=$(echo drs-logs-"`date +"%d-%m-%Y"`")
 #    tar -czvf $archive_logs_name.tar.gz -C $DRS_LOGS_DEST $DRS_LOGS_DEST
+  echo "tar -czvf $archive_logs_name.tar.gz -C $DRS_LOGS_DEST $DRS_LOGS_DEST"
 }
 
 get_ha_logs () {
