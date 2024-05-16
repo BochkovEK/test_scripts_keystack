631acabe8406fc8d25d82e376cb7a5bc2797d294
ebochkov <ebochkov@itkey.com>
2024-05-16 14:13:09 +0300

    wip test_scripts

--- self_signed_certs/certs_envs.sh
+++ self_signed_certs/certs_envs.sh
@@ -1,13 +1,13 @@
 #!/bin/env bash
 
 #Envs list
-export CERTS_DIR=$HOME/central_auth_service
-export OUTPUT_CERTS_DIR=$HOME/certs
-export DOMAIN=test.domain
-export CA_IP=10.224.129.234
-export LCM_GITLAB_NAME=ks-lcm
-export LCM_NEXUS_NAME=nexus
-export LCM_VAULT_NAME=vault
-export LCM_NETBOX_NAME=netbox
+#export CERTS_DIR=$HOME/central_auth_service
+#export OUTPUT_CERTS_DIR=$HOME/certs
+#export DOMAIN=test.domain
+#export CA_IP=10.224.129.234
+#export LCM_GITLAB_NAME=ks-lcm
+#export LCM_NEXUS_NAME=nexus
+#export LCM_VAULT_NAME=vault
+#export LCM_NETBOX_NAME=netbox
 
-LCM_NETBOX_NAME=netbox
+LCM_NETBOX_NAME="netbox"
