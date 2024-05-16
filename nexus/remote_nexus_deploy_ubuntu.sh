2c9a297f24bb36befbcb881d946bba3dbe57dce0
ebochkov <ebochkov@itkey.com>
2024-05-16 13:33:58 +0300

    wip test_scripts

--- nexus/remote_nexus_deploy_ubuntu.sh
+++ nexus/remote_nexus_deploy_ubuntu.sh
@@ -10,7 +10,7 @@ REMOTE_NEXUS=remote-nexus
 
 #Script_dir, current folder
 script_dir=$(dirname $0)
-parentdir="$(dirname "$script_dir")"
+parentdir=$(dirname "$script_dir")
 
 source $parentdir/cert_envs
 
