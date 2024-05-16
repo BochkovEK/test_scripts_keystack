4929e239b94579f6c49589441cb962df3c342983
ebochkov <ebochkov@itkey.com>
2024-05-16 14:56:29 +0300

    wip test_scripts

--- nexus/remote_nexus_deploy_ubuntu.sh
+++ nexus/remote_nexus_deploy_ubuntu.sh
@@ -48,6 +48,6 @@ bash $parentdir/self_signed_certs/generate_self_signed_certs.sh
 
 sed -i "s/DOMAIN/$DOMAIN/g" $script_dir/nginx_https.conf
 sed -i "s/LCM_NEXUS_NAME/$LCM_NEXUS_NAME/g" $script_dir/nginx_https.conf
-sed -i -e "s##OUTPUT_CERTS_DIR##$OUTPUT_CERTS_DIR##g" $script_dir/nginx_https.conf
+sed -i -e "s@OUTPUT_CERTS_DIR@$OUTPUT_CERTS_DIR@g" $script_dir/nginx_https.conf
 
 #docker compose -f $script_dir/docker-compose.yaml up -d
