11185197650f97f2aa8487fd854341f731e92dbe
ebochkov <ebochkov@itkey.com>
2024-05-16 13:32:55 +0300

    wip test_scripts

--- nexus/remote_nexus_deploy_ubuntu.sh
+++ nexus/remote_nexus_deploy_ubuntu.sh
@@ -4,6 +4,8 @@
 
 #!!! Script сhanging LCM_NEXUS_NAME into $parentdir/self_signed_certs/certs_envs to $REMOTE_NEXUS
 
+[[ -z $DEBUG ]] && DEBUG="true"
+
 REMOTE_NEXUS=remote-nexus
 
 #Script_dir, current folder
@@ -12,8 +14,27 @@ parentdir="$(dirname "$script_dir")"
 
 source $parentdir/cert_envs
 
+  [ "$DEBUG" = true ] && echo -e "
+  [DEBUG]
+  script_dir: $script_dir
+  parentdir: $parentdir
+  CERTS_DIR: $CERTS_DIR
+  OUTPUT_CERTS_DIR: $OUTPUT_CERTS_DIR
+  DOMAIN: $DOMAIN
+  CA_IP: $CA_IP
+  LCM_NEXUS_NAME: $LCM_NEXUS_NAME
+  LCM_GITLAB_NAME: $LCM_GITLAB_NAME
+  LCM_VAULT_NAME: $LCM_VAULT_NAME
+  LCM_NETBOX_NAME: $LCM_NETBOX_NAME
+  "
+
 lcm_nexus_name_string=$(cat $parentdir/self_signed_certs/certs_envs|grep -m "LCM_NEXUS_NAME")
 
+  [ "$DEBUG" = true ] && echo -e "
+  [DEBUG]
+  lcm_nexus_name_string: $lcm_nexus_name_string
+  REMOTE_NEXUS: $REMOTE_NEXUS
+  "
 sed -i "s/$lcm_nexus_name_string/LCM_NEXUS_NAME=$REMOTE_NEXUS/" $parentdir/self_signed_certs/certs_envs
 
 bash $parentdir/self_signed_certs/generate_self_signed_certs.sh
