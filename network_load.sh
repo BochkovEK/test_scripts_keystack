28c4551ec25db50aac6ac55eea269a4c1da235de
ebochkov <ebochkov@itkey.com>
2024-05-30 12:43:56 +0300

    wip test_scripts

--- network_load.sh
+++ network_load.sh
@@ -11,6 +11,8 @@
 #load="on"
 #OPENRC_PATH="./openrc"
 
+#Script_dir, current folder
+script_dir=$(dirname $0)
 
 [[ -z $OPENRC_PATH ]] && OPENRC_PATH=$HOME/openrc
 [[ -z $KEY_NAME ]] && KEY_NAME="key_test"
@@ -24,11 +26,11 @@ network_stress() {
     case $LOAD in
         on)
             echo "Starting network load on $VM_IP..."
-            ssh -t -o StrictHostKeyChecking=no -i $key_name ubuntu@"$VM_IP" "sudo sh -c 'echo \"@reboot root ping -f -s 1024 8.8.8.8\" >> /etc/crontab && reboot'"
+            ssh -t -o StrictHostKeyChecking=no -i $script_dir/$key_name ubuntu@"$VM_IP" "sudo sh -c 'echo \"@reboot root ping -f -s 1024 8.8.8.8\" >> /etc/crontab && reboot'"
             ;;
         off)
             echo "Stopping network load on $VM_IP..."
-            ssh -t -o StrictHostKeyChecking=no -i $key_name ubuntu@"$VM_IP" "sudo sh -c 'sed -i '/ping/d' /etc/crontab && reboot'"
+            ssh -t -o StrictHostKeyChecking=no -i $script_dir/$key_name ubuntu@"$VM_IP" "sudo sh -c 'sed -i '/ping/d' /etc/crontab && reboot'"
             ;;
         *) echo "load equals invalid value: $LOAD";;
     esac
