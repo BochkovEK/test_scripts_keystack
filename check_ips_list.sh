21998beb707341100c25bdf53bc3393d019df98e
ebochkov <ebochkov@itkey.com>
2024-06-13 17:23:43 +0300

    wip test_scripts

--- check_ips_list.sh
+++ check_ips_list.sh
@@ -10,7 +10,7 @@ while [[ $i -le $END ]]; do
 	echo "$BASE_IP.$i"
 	ping -c 2 $BASE_IP.$i &> /dev/null
 	if ping -c 2 $IP &> /dev/null; then
-    printf "%40s\n" "${green}There is a connection with $IP - success${normal}"
+    printf "%40s\n" "${green}There is a connection with $BASE_IP.$i - success${normal}"
 #    [ "$ONLY_PING" == "false" ] && { ssh -t -o StrictHostKeyChecking=no -i $script_dir/$KEY_NAME $VM_USER@$IP "$COMMAND_STR"; }
   else
     printf "%40s\n" "${red}No connection with $IP - error!${normal}"
