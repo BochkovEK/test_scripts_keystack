f864dbf74b102d9c9142f234a99690d210371e8d
ebochkov <ebochkov@itkey.com>
2024-06-21 14:22:07 +0300

    wip test_scripts

--- iso_bootable_image_create_in_openstack.sh
+++ iso_bootable_image_create_in_openstack.sh
@@ -36,14 +36,16 @@ check_and_source_openrc_file () {
 check_and_source_openrc_file
 iso=$script_dir/gpart-live.iso
 if [ -f "$iso" ]; then
-  echo "$iso exists."
+  echo "$iso exists in $script_dir"
 else
+  echo "download $iso for repo.itkey"
   wget https://repo.itkey.com/repository/images/gparted-live-1.5.0-6-amd64.iso -O $iso
 fi
 
 iso_in_openstack=$(openstack image list|grep gpart-live.iso)
 
 if [ -z $iso_in_openstack ]; then
+  echo "create bootable iso image in openstack"
   openstack image create gpart-live.iso \
     --file gpart-live.iso \
     --disk-format iso \
@@ -51,6 +53,8 @@ if [ -z $iso_in_openstack ]; then
     --property hw_rescue_device=cdrom \
     --property hw_rescue_device=cdrom \
     --property hw_rescue_bus=usb
+else
+  echo "bootable iso image already exists in openstack"
 fi
 
 
