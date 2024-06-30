be53fe15f9c9361e85e84403cc048bef7ead77df
ebochkov <ebochkov@itkey.com>
2024-06-30 17:15:59 +0300

    wip test_scripts

--- terraform/examples/example_1/provider.tf
+++ terraform/examples/example_1/provider.tf
@@ -1,17 +1,17 @@
-terraform {
-required_version = ">= 0.14.0"
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.48.0"
-    }
-  }
-}
- 
-provider "openstack" {
-
-    cloud = "openstack"
-    endpoint_overrides = {
-        "volumev3" = var.volumev3
-    }
-}
+#terraform {
+#required_version = ">= 0.14.0"
+#  required_providers {
+#    openstack = {
+#      source  = "terraform-provider-openstack/openstack"
+#      version = "~> 1.48.0"
+#    }
+#  }
+#}
+#
+#provider "openstack" {
+#
+#    cloud = "openstack"
+#    endpoint_overrides = {
+#        "volumev3" = var.volumev3
+#    }
+#}
