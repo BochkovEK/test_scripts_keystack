7249809e6ef5406a33058145fc534bead609487c
ebochkov <ebochkov@itkey.com>
2024-06-30 19:33:11 +0300

    wip test_scripts

--- terraform_2/modules/instances/variables.tf
+++ terraform_2/modules/instances/variables.tf
@@ -1,29 +1,23 @@
 variable "vm_name" {
-  description = "VM name"
   type        = string
 }
 
 variable "image_name" {
-  description = "Image name"
   type        = string
 }
 
 variable "flavor_name" {
-  description = "Flavor name"
   type        = string
 }
 
 variable "keypair_name" {
-  description = "Key pair name"
   type        = string
 }
 
 variable "network_name" {
-  description = "Network name"
   type        = string
 }
 
 variable "security_groups" {
-  description = "Security group name"
   type        = string
 }
