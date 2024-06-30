a0f8daf2a94f394c38181b67b112b3c8db3dc443
ebochkov <ebochkov@itkey.com>
2024-06-30 22:16:37 +0300

    wip test_scripts

--- terraform_2/modules/instances/provider.tf
+++ terraform_2/modules/instances/provider.tf
@@ -9,7 +9,7 @@ required_version = ">= 0.14.0"
 }
 
 provider "openstack" {
-     cloud = "/root/test_scripts_keystack/terraform_2/clouds.yml"
+     cloud = "clouds"
 #    endpoint_overrides = {
 #        "volumev3" = var.volumev3
 #    }
