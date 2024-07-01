0f840d1d7b02366c8a8079cd63d4161c0e957149
ebochkov <ebochkov@itkey.com>
2024-07-01 17:39:37 +0300

    wip test_scripts

--- terraform_2/modules/instances/locals.tf
+++ terraform_2/modules/instances/locals.tf
@@ -5,8 +5,8 @@ locals {
     for instance_key, instance in var.VMs : [
       for iter in range(1,instance.vm_qty+1) : {
         name             = format("%s-%02d", instance_key, iter)
-        image_name                  = each.value.image_name == null ? var.default_image_name : each.value.image_name
-        flavor_name                 = each.value.flavor_name == null ? var.default_flavor_name : each.value.flavor_name
+        image_name                  = instance.image_name == null ? var.default_image_name : instance.image_name
+        flavor_name                 = instance.flavor_name == null ? var.default_flavor_name : instance.flavor_name
         key_pair                    = each.value.keypair_name
         security_groups             = each.value.security_groups
         availability_zone_hints     = each.value.az_hint
