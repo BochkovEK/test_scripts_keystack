e1e98f837bc76f367425e3c4bc7897b57b3141f0
ebochkov <ebochkov@itkey.com>
2024-07-01 18:30:28 +0300

    wip test_scripts

--- terraform_2/modules/instances/locals.tf
+++ terraform_2/modules/instances/locals.tf
@@ -5,13 +5,13 @@ locals {
     for instance_key, instance in var.VMs : [
       for iter in range(1,instance.vm_qty+1) : {
         name              = format("%s-%02d", instance_key, iter)
-        image_name        = try(instance.image_name, var.default_image_name) #instance.image_name == null ? var.default_image_name : instance.image_name
-        flavor_name       = instance.flavor_name == null ? var.default_flavor_name : instance.flavor_name
-        keypair_name      = instance.keypair_name
-        security_groups   = instance.security_groups
-        az_hint           = instance.az_hint
-        volume_size       = instance.volume_size == null ? var.default_volume_size : instance.volume_size
-        network_name      = instance.network_name == null ? var.default_network_name : instance.network_name
+        image_name        = try(instance.image_name, var.default_image_name) # instance.image_name == null ? var.default_image_name : instance.image_name
+        flavor_name       = try(instance.flavor_name, var.default_flavor_name) # == null ? var.default_flavor_name : instance.flavor_name
+        keypair_name      = try(instance.keypair_name, null)
+        security_groups   = try(instance.security_groups, null)
+        az_hint           = try(instance.az_hint, null)
+        volume_size       = try(instance.volume_size, null) # == null ? var.default_volume_size : instance.volume_size
+        network_name      = try(instance.network_name, null) # == null ? var.default_network_name : instance.network_name
       }
     ]
   ])
