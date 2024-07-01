d135925c59f69db642085be584ae1545ec2aef34
ebochkov <ebochkov@itkey.com>
2024-07-01 17:45:54 +0300

    wip test_scripts

--- terraform_2/modules/instances/locals.tf
+++ terraform_2/modules/instances/locals.tf
@@ -7,10 +7,10 @@ locals {
         name             = format("%s-%02d", instance_key, iter)
         image_name                  = instance.image_name == null ? var.default_image_name : instance.image_name
         flavor_name                 = instance.flavor_name == null ? var.default_flavor_name : instance.flavor_name
-        key_pair                    = instance.value.keypair_name
-        security_groups             = instance.value.security_groups
-        availability_zone_hints     = instance.value.az_hint
-        volume_size                 = instance.value.volume_size == null ? var.default_volume_size : instance.volume_size
+        key_pair                    = instance.keypair_name
+        security_groups             = instance.security_groups
+        availability_zone_hints     = instance.az_hint
+        volume_size                 = instance.volume_size == null ? var.default_volume_size : instance.volume_size
         network_name                = instance.network_name == null ? var.default_network_name : instance.network_name
       }
     ]
