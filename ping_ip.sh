20d41de5e5f49aa80b75b9dc0e56a45bc91b1a9e
ebochkov <ebochkov@itkey.com>
2024-07-24 11:49:33 +0300

    wip

--- ping_ip.sh
+++ ping_ip.sh
@@ -11,14 +11,15 @@ script_dir=$(dirname $0)
 IP=$1
 [[ -z ${IP} ]] && { printf "%40s\n" "${red}Pleas define IP with starter script parameters${normal}"; exit 1; }
 
-echo "========= Start ping to $IP at $(date '+%d/%m/%Y %H:%M:%S') =========" >> $script_dir/ping_states_with_$IP.log
+echo -e "\n========= Start ping to $IP at $(date '+%d/%m/%Y %H:%M:%S') =========\n" >> $script_dir/ping_states_with_$IP.log
 
 while true; do
   if ping -c 2 $IP &> /dev/null; then
     printf "%40s\n" "${green}There is a connection with $IP - success${normal}"
   else
     printf "%40s\n" "${red}No connection with $IP - error!${normal}"
-    echo date '+%d/%m/%Y %H:%M:%S' >> $script_dir/ping_states_with_$IP.log
+    dt=$(date '+%d/%m/%Y %H:%M:%S')
+    echo $dt >> $script_dir/ping_states_with_$IP.log
     echo "No connection with $IP - error!" >> $script_dir/ping_states_with_$IP.log
   fi
   sleep 1
