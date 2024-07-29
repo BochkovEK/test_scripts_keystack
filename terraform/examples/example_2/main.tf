df8fba698b6d877d16992766d15f05bef66618b9
ebochkov <ebochkov@itkey.com>
2024-07-29 17:21:44 +0300

    wip

--- terraform/examples/example_2/main.tf
+++ terraform/examples/example_2/main.tf
@@ -3,7 +3,7 @@ resource "openstack_compute_instance_v2" "vm" {
 #  image_name                  = cirros-0.5.2-x86_64-disk
   flavor_name                 = "g1-cpu-1-1"
   key_pair                    = "key_test"
-  security_groups             = "allow_all"
+  security_groups             = ["allow_all"]
   availability_zone_hints     = "cpu:cdm-bl-pca11"
   count                       = 5
   metadata = {
