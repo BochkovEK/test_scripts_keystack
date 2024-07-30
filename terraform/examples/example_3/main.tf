b227bcf76b563fd0a65516b52361cc10ef90b2a1
ebochkov <ebochkov@itkey.com>
2024-07-30 14:11:25 +0300

    wip

--- terraform/examples/example_3/main.tf
+++ terraform/examples/example_3/main.tf
@@ -18,7 +18,7 @@ resource "openstack_compute_instance_v2" "fc_hdd" {
   }
   block_device {
 #    uuid                  = openstack_blockstorage_volume_v3.fc_hdd_sda[count.index].id
-    name         = "fc_hdd_boot"
+#    name         = "fc_hdd_boot"
     uuid                  = data.openstack_images_image_v2.image.id
     volume_size           = 1
     source_type           = "image"
