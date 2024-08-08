85c4dda50d139e64a3ee5fa67765580bdaae6a7e
ebochkov <ebochkov@itkey.com>
2024-08-08 12:46:41 +0300

    wip

--- terraform/modules/instances/variables.tf
+++ terraform/modules/instances/variables.tf
@@ -26,11 +26,11 @@ variable "default_disk" {
   }
 }
 
-#variable "default_volume_size" {
-#  description = "Default volume size"
-#  type        = number
-#  default = 5
-#}
+variable "default_boot_volume_size" {
+  description = "Default volume size"
+  type        = number
+  default = 5
+}
 
 variable "default_network_name" {
   description = "Default network name"
