4fa146df5db9d4420ae4a45e07b857dddd440a01
ebochkov <ebochkov@itkey.com>
2024-08-09 07:32:20 +0300

    wip

--- terraform/modules/instances/instance.tf
+++ terraform/modules/instances/instance.tf
@@ -26,7 +26,7 @@ resource "openstack_compute_instance_v2" "vm" {
  block_device {
 #    uuid                  = openstack_blockstorage_volume_v3.fc_hdd_sda[count.index].id
 #    name         = "fc_hdd_boot"
-    uuid                  = data.openstack_images_image_v2.image_id[each.key].id
+    uuid                  = "volume-${each.value.base_name}-${data.openstack_images_image_v2.image_id[each.key].name}"
     volume_size           = each.value.boot_volume_size
     source_type           = "image"
     boot_index            = 0
