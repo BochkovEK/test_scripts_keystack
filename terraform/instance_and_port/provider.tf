c71dcdbcc47928ea8d0ee99b08b2601207741688
ebochkov <ebochkov@itkey.com>
2024-08-26 16:25:46 +0300

    wip

--- terraform/instance_and_port/provider.tf
+++ terraform/instance_and_port/provider.tf
@@ -8,9 +8,18 @@ required_version = ">= 0.14.0"
   }
 }
 
+# Configure the OpenStack Provider
 provider "openstack" {
-     cloud = "openstack"
-#    endpoint_overrides = {
-#        "volumev3" = var.volumev3
-#    }
+  user_name   = "admin"
+  tenant_name = "admin"
+  password    = "d1D73NQjpO5GuKlFspR0BX3PAt8fF55BZThs3sYf"
+  auth_url    = "https://int.ebochkov.test.domain:5000/v3"
+  region      = "ebochkov"
+  cacert_file = "/root//root/ca-bundle.crt"
 }
+#provider "openstack" {
+#     cloud = "openstack"
+##    endpoint_overrides = {
+##        "volumev3" = var.volumev3
+##    }
+#}
