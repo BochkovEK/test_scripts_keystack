07ef78dbce4f1c27854f75316c60de68df502947
ebochkov <ebochkov@itkey.com>
2024-09-19 13:19:14 +0300

    wip

--- installer/start.sh
+++ installer/start.sh
@@ -10,7 +10,8 @@ parent_dir=$(dirname "$script_dir")
 [[ -z $CERTS_FOLDER ]] && CERTS_FOLDER="$HOME/certs"
 [[ -z $RELEASE_URL ]] && RELEASE_URL="https://repo.itkey.com/repository/k-install/installer-ks2024.3-rc1-sberlinux-offline.tgz"
 [[ -z $INSTALLER_CONF ]] && INSTALLER_CONF="Client_certs_Nexus_LDAP_Gitlab_Netbox_installer_envs"
-
+[[ -z $INIT_INSTALLER_FOLDER ]] && INIT_INSTALLER_FOLDER="$HOME/installer"
+[[ -z $INIT_INSTALLER_BACKUP_FOLDER ]] && INIT_INSTALLER_BACKUP_FOLDER="$HOME/installer_backup"
 
 bash $parent_dir/utils/install_wget.sh
 release_tar=$(echo "${RELEASE_URL##*/}")
@@ -18,11 +19,16 @@ echo "release_tar: $release_tar"
 if [ ! -f ~/$release_tar ]; then
   wget $RELEASE_URL -P $HOME/
 fi
-if [ ! -d "$HOME/installer" ]; then
-  echo "Untar installer archive..."
-  tar -xf $HOME/*.tgz -C $HOME/
+if [ ! -d $INIT_INSTALLER_FOLDER ]; then
+  if [ ! -d $INIT_INSTALLER_BACKUP_FOLDER ]; then
+    echo "Untar installer archive..."
+    tar -xf $HOME/*.tgz -C $HOME/
+  else
+    echo "Copy init installer folder from backup folder"
+    cp -r $INIT_INSTALLER_BACKUP_FOLDER $INIT_INSTALLER_FOLDER
 fi
 if [ ! -d "$HOME/installer_backup" ]; then
+  echo "Create backup folder"
   cp -r ~/installer ~/installer_backup
 fi
 
