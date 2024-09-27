34cad4df99deca024d71d64cd3831522621f9391
ebochkov <ebochkov@itkey.com>
2024-09-27 17:15:59 +0300

    wip

--- utils/yes_no_answer.sh
+++ utils/yes_no_answer.sh
@@ -5,25 +5,25 @@
 [[ -z $DEBUG ]] && DEBUG="sosa"
 [[ -z $TS_YES_NO_QUESTION ]] && TS_YES_NO_QUESTION="foo"
 
-#echo $DEBUG
-
-yes_no_answer () {
-  while true; do
-    read -p "$TS_YES_NO_QUESTION" yn
-    yn=${yn:-"Yes"}
-#    echo $yn
-    case $yn in
-        [Yy]* ) echo "true"; break;;
-        [Nn]* ) echo "false"; break ;;
-        * ) echo "Please answer yes or no.";;
-    esac
-  done
-#  export TS_YES_NO_QUESTION='<Empty yes/no question>'
-}
-
 echo $DEBUG
-yes_no_answer
-[ "$DEBUG" = true ] && echo -e "
-  [DEBUG]
-  TS_YES_NO_QUESTION:   $TS_YES_NO_QUESTION
-"
+
+#yes_no_answer () {
+#  while true; do
+#    read -p "$TS_YES_NO_QUESTION" yn
+#    yn=${yn:-"Yes"}
+##    echo $yn
+#    case $yn in
+#        [Yy]* ) echo "true"; break;;
+#        [Nn]* ) echo "false"; break ;;
+#        * ) echo "Please answer yes or no.";;
+#    esac
+#  done
+##  export TS_YES_NO_QUESTION='<Empty yes/no question>'
+#}
+#
+#echo $DEBUG
+#yes_no_answer
+#[ "$DEBUG" = true ] && echo -e "
+#  [DEBUG]
+#  TS_YES_NO_QUESTION:   $TS_YES_NO_QUESTION
+#"
