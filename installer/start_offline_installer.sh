14b925db0321ebf6c7caeaff61cd19d6f9823ef4
ebochkov <ebochkov@itkey.com>
2024-10-10 18:46:51 +0300

    wip

--- installer/start_offline_installer.sh
+++ installer/start_offline_installer.sh
@@ -58,10 +58,8 @@ select_config_file () {
           echo 'Invalid choice, try again' >&2
       else
         config_file=${files[$REPLY]}
-        echo "
-        Installer config selected:
-        $config_file
-        "
+        echo -e "\nInstaller config selected:"
+        echo -e "$config_file\n"
         break
       fi
   done
