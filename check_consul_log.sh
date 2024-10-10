e1c61f22624431665c764a6dc8ccc35d351ea369
ebochkov <ebochkov@itkey.com>
2024-10-10 11:15:49 +0300

    wip

--- check_consul_log.sh
+++ check_consul_log.sh
@@ -164,7 +164,7 @@ do
         -e 's/\(.*WARNING.*\)/\o033[33m\1\o033[39m/' \
         -e 's/\(.*Starting fence.*\)/\o033[33m\1\o033[39m/'
 
-    ssh -o StrictHostKeyChecking=no "$NODE_NAME" 'echo -e "\033[0;35m$(date)\033[0m
+    ssh -o StrictHostKeyChecking=no "$ctrl" 'echo -e "\033[0;35m$(date)\033[0m
 \033[0;35mLogs from: $(hostname)\033[0m
 \033[0;35mFor check this log: \033[0m
 \033[0;35mssh $(hostname) less /var/log/kolla/autoevacuate.log | less\033[0m"'
