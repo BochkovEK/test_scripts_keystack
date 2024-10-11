4c0cebd75fe97a30bdba2ab002b05abd28517d1f
ebochkov <ebochkov@itkey.com>
2024-10-11 09:05:44 +0300

    wip

--- terraform/terraform_deploy.sh
+++ terraform/terraform_deploy.sh
@@ -174,8 +174,8 @@ for image_name in "${public_images_list[@]}"; do
   image_name_cut=$(echo "${image_name##*/}")
   image_source=$(echo "${image_name%/*}")
 #  echo $image_source $image_name_cut
+#  exit 1
   export IMAGE_SOURCE=$image_source
-  exit 1
   if ! bash $utils_dir/openstack/create_image.sh $image_name_cut; then
     exit 1
   fi
