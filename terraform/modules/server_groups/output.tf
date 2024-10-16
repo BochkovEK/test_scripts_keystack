d5bafce2afa9d9f7f6d967ab18f715bd29785db0
ebochkov <ebochkov@itkey.com>
2024-10-16 20:43:01 +0300

    wip

--- terraform/modules/server_groups/output.tf
+++ terraform/modules/server_groups/output.tf
@@ -1,5 +1,5 @@
 output "server_group_id" {
 #  for_each = var.server_groups
-  value = { for k, servergroup in openstack_compute_servergroup_v2.servergroup.id : k => server_group_id
+  value = { for k, servergroup in openstack_compute_servergroup_v2.servergroup.id : k => servergroup.id
   }
 }
