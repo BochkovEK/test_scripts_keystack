45d650aeb19dc2a6b755c7d4351aa36b14e2f896
ebochkov <ebochkov@itkey.com>
2024-10-21 00:57:58 +0300

    wip

--- start_stress.sh
+++ start_stress.sh
@@ -137,6 +137,7 @@ check_vm () {
   export HYPERVISOR_NAME=$HYPERVISOR_NAME
   export PROJECT=$PROJECT
   export VMs_IPs=$VMs_IPs
+  export ONLY_CHECK=true
   [ "$TS_DEBUG" = true ] && { debug_string="-v"; }
   if ! bash $script_dir/$check_vm_script $debug_string; then
     echo -E "${red}VMs are not ready to start stress - error${normal}"
