09524890c8ca5732e837ced906fbc76cf6f78c0d
ebochkov <ebochkov@itkey.com>
2024-10-24 18:08:03 +0300

    wip

--- terraform/examples/create_vms_with_module/provider.tf
+++ terraform/examples/create_vms_with_module/provider.tf
@@ -10,18 +10,18 @@ required_version = ">= 0.14.0"
 
 provider "openstack" {
 #     cloud = "openstack"
-  user_name: "admin"
-  tenant_name: "admin"
-  password: "56OnOHXYLVdsS5a46cdFXct2c9kI2vzYK3uivdFd"
-  auth_url: "https://int.ebochkov.test.domain:5000"
-  region_name: "ebochkov"
-  user_domain_name: "Default"
+  user_name             = "admin"
+  tenant_name           = "admin"
+  password              = "56OnOHXYLVdsS5a46cdFXct2c9kI2vzYK3uivdFd"
+  auth_url              = "https://int.ebochkov.test.domain:5000"
+  region_name           = "ebochkov"
+  user_domain_name      = "Default"
   project_id: bee56048988140faa81bc8d30fc07b1f
-  interface: "public"
-  identity_api_version: 3
+  interface             = "public"
+  identity_api_version  = 3
       #cacert: "~/bundle.crt"
       #insecure: true
-  insecure = "true"
+  insecure              = "true"
 }
 
 #    endpoint_overrides = {
