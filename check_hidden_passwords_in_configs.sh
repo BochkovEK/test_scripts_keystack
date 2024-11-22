52ff4432585a90007d194bcaaf62105f555412f1
ebochkov <ebochkov@itkey.com>
2024-11-22 15:48:30 +0300

    wip

--- check_hidden_passwords_in_configs.sh
+++ check_hidden_passwords_in_configs.sh
@@ -64,7 +64,7 @@ read_conf () {
   bash $command_on_nodes_script_name -nt 1 -c "cat $2 | grep -E 'password|\[castellan_configsource\]'| \
     sed --unbuffered \
       -e 's/\(.*\[castellan_configsource\].*\)/\o033[32m\1 - ok\o033[39m/' \
-      -e 's/\(.*\No such file or directory.*\")/\o033[31m\1 - ok\o033[39m/'"
+      -e 's/\(.*\No such file or directory.*\)/\o033[31m\1 - ok\o033[39m/'"
 }
 
 Check_configs_on_controls () {
