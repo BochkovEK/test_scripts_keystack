21871c64bc31debd97181d10d60238b519a422f8
ebochkov <ebochkov@itkey.com>
2024-11-26 12:10:48 +0300

    wip

--- utils/get_nodes_list.sh
+++ utils/get_nodes_list.sh
@@ -104,7 +104,8 @@ node_type_func () {
 
 check_openstack_cli () {
 #  echo "check"
-  export ASK_TO_INSTALL=false
+  export DONT_ASK=true
+  export DONT_INSTALL=true
   if bash $utils_dir/$check_openstack_cli_script &> /dev/null; then
     check_and_source_openrc_file
     get_list_from_compute_service
