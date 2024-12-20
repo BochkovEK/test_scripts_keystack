77f1bb2b93b3059115b3bdde1111cd0cdf89abfc
ebochkov <ebochkov@itkey.com>
2024-12-20 07:26:13 +0300

    wip

--- match_script.py
+++ match_script.py
@@ -16,11 +16,12 @@ with open(to_find_file_path) as to_find_file:
     to_find_lines = [line.rstrip() for line in to_find_file]
 
 for find in to_find_lines:
-    # print(find)
+    print(find)
     for text_line in text_lines:
-        # print(text_line)
+        print(text_line)
         if find in text_line:
             match.append(text_line)
+            break
 
 # Set the mode in open() to "a" (append) instead of "w" (write):
 for line in match:
