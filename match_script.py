abbf3667ebfc2c36d461a35105a2c696bc2a839d
ebochkov <ebochkov@itkey.com>
2024-12-20 15:36:49 +0300

    wip

--- match_script.py
+++ match_script.py
@@ -1,15 +1,22 @@
 import sys
+import os
 
 # small file
 what_find = sys.argv[1]
 # big file
 where_find = sys.argv[2]
-
-# text_file_path = "./text.txt"
-
-# to_find_file_path = "./to_find.txt"
 match_file_path = "./match.txt"
+debug = False
 match = []
+
+if os.environ['TS_DEBUG'] == "true":
+    debug = True
+
+if os.environ['OUTPUT_MATCH_FILE']:
+    match_file_path = os.environ['OUTPUT_MATCH_FILE']
+
+if sys.argv[3]:
+    match_file_path = sys.argv[3]
 
 with open(what_find) as what_find_file:
     find_lines = [line.rstrip() for line in what_find_file]
@@ -18,16 +25,19 @@ with open(where_find) as where_find_file:
     where_find_lines = [line.rstrip() for line in where_find_file]
 
 for find in find_lines:
-    # print(find)
+    if debug:
+        print(find)
     for where_find_line in where_find_lines:
-        # print(where_find_line)
+        if debug:
+            print(where_find_line)
         if find in where_find_line:
             match.append(where_find_line)
             break
 
 # Set the mode in open() to "a" (append) instead of "w" (write):
 for line in match:
-    print(line)
+    if debug:
+        print(line)
     with open(match_file_path, "a") as f:
         f.write(line+"\n")
 
