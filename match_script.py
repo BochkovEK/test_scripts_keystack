ff212afdd711f17ca868a8ab05896f326972521c
ebochkov <ebochkov@itkey.com>
2024-12-26 12:41:45 +0300

    wip

--- match_script.py
+++ match_script.py
@@ -7,7 +7,18 @@ what_find = sys.argv[1]
 where_find = sys.argv[2]
 match_file_path = "./match.txt"
 debug = False
+white_list = True
 match = []
+
+try:
+    os.environ['WHITE_LIST']
+except KeyError:
+    pass
+else:
+    if os.environ['WHITE_LIST'] == "true":
+        white_list = True
+    else:
+        white_list = False
 
 try:
     os.environ['TS_DEBUG']
@@ -39,15 +50,27 @@ with open(what_find) as what_find_file:
 with open(where_find) as where_find_file:
     where_find_lines = [line.rstrip() for line in where_find_file]
 
-for find in find_lines:
-    if debug:
-        print(find)
-    for where_find_line in where_find_lines:
+if white_list:
+    for find in find_lines:
         if debug:
-            print(where_find_line)
-        if find in where_find_line:
-            match.append(where_find_line)
-            break
+            print(find)
+        for where_find_line in where_find_lines:
+            if debug:
+                print(where_find_line)
+            if find in where_find_line:
+                match.append(where_find_line)
+                break
+else:
+    for find in find_lines:
+        if debug:
+            print(find)
+        for where_find_line in where_find_lines:
+            if debug:
+                print(where_find_line)
+            if find not in where_find_line:
+                match.append(where_find_line)
+                break
+
 
 # Set the mode in open() to "a" (append) instead of "w" (write):
 for line in match:
