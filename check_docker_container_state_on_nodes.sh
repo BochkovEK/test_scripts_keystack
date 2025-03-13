1cae25a620abc718d93c33a45d11eef0cc3367a4
ebochkov <ebochkov@itkey.com>
2025-03-13 18:40:29 +0300

    wip

--- check_docker_container_state_on_nodes.sh
+++ check_docker_container_state_on_nodes.sh
@@ -144,7 +144,8 @@ get_nodes_list
   echo "UNHEALTHY: $UNHEALTHY";
   }
 
-grep_string="| grep -E \"$UNHEALTHY\\s+$CONTAINER_NAME\""
+#grep_string="| grep -E \"$UNHEALTHY\\s+$CONTAINER_NAME\"|starting"
+grep_string="| grep -E \"$CONTAINER_NAME|starting\""
 
 [ "$TS_DEBUG" = true ] && echo -e "
   [DEBUG]
@@ -167,6 +168,7 @@ for host in "${NODES[@]}"; do
         -e 's/\(.*restarting.*\)/\o033[31m\1\o033[39m/' \
         -e 's/\(.*(healthy).*\)/\o033[92m\1\o033[39m/' \
         -e 's/\(.*Up.*\)/\o033[92m\1\o033[39m/' \
+        -e 's/\(.*starting.*\)/\o033[33m\1\o033[33m/'\
         -e 's/\(.*Less than.*\)/\o033[33m\1\o033[39m/'
   else
     printf "%40s\n" "${red}No connection with $host - error!${normal}"
