85e1e97d27282d957388ed579a26bbe0b15587d1
ebochkov <ebochkov@itkey.com>
2025-03-18 11:56:52 +0300

    wip

--- check_docker_container_state_on_nodes.sh
+++ check_docker_container_state_on_nodes.sh
@@ -135,6 +135,7 @@ for host in "${NODES[@]}"; do
 #    ssh -o StrictHostKeyChecking=no $host docker ps $grep_string \
     ssh -o StrictHostKeyChecking=no $host docker ps \
       |sed --unbuffered \
+        -e 's/\(.*(unhealthy).*\)/\o033[31m\1\o033[39m/' \
         -e 's/\(.*second.*\)/\o033[33m\1\o033[39m/' \
         -e 's/\(.*Less than.*\)/\o033[33m\1\o033[39m/' \
         -e 's/\(.*(healthy).*\)/\o033[92m\1\o033[39m/' \
@@ -143,7 +144,6 @@ for host in "${NODES[@]}"; do
         -e 's/\(.*weeks.*\)/\o033[92m\1\o033[39m/' \
         -e 's/\(.*hours.*\)/\o033[92m\1\o033[39m/' \
         -e 's/\(.*starting).*\)/\o033[33m\1\o033[39m/'\
-        -e 's/\(.*(unhealthy).*\)/\o033[31m\1\o033[39m/' \
         -e 's/\(.*restarting.*\)/\o033[31m\1\o033[39m/'
 
 #        -e 's/\(.*Up.*\)/\o033[92m\1\o033[39m/' \
