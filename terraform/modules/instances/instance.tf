9d5e7970594af8d06a19e028ba85ae4304936d1f
ebochkov <ebochkov@itkey.com>
2025-04-22 10:19:08 +0300

    wip

--- terraform/modules/instances/instance.tf
+++ terraform/modules/instances/instance.tf
@@ -156,14 +156,14 @@ resource "openstack_compute_secgroup_v2" "secgroup" {
   cidr = "0.0.0.0/0"
  }
  rule {
-  from_port = -1
-  to_port = -1
+  from_port = 0
+  to_port = 65535
   ip_protocol = "udp"
   cidr = "0.0.0.0/0"
  }
  rule {
-  from_port = -1
-  to_port = -1
+  from_port = 0
+  to_port = 65535
   ip_protocol = "tcp"
   cidr = "0.0.0.0/0"
  }
