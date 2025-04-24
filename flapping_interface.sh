b20f287e0652cac2a27c9589ead48a2d1402291a
ebochkov <ebochkov@itkey.com>
2025-04-24 08:52:45 +0300

    wip

--- flapping_interface.sh
+++ flapping_interface.sh
@@ -3,32 +3,42 @@
 #Colors
 normal=$(tput sgr0)
 yellow=$(tput setaf 3)
-interface_name="eth0"
+default_interface_name="eth0"
+default_number_of_cycle=20
+default_sleep_time=5
+
+[[ -z $TS_NUMBER_OF_CYCLES ]] && TS_NUMBER_OF_CYCLES=$default_number_of_cycle
+[[ -z $TS_SLEEP_TIME ]] && TS_SLEEP_TIME=$default_sleep_time
 
 echo "Start flapping interface test script..."
 echo -e "${yellow}[WARNING]: This script must be executed on the node where the interface is being disabled(flapping).${normal}"
 if [ -z "$1" ]; then
-    if [ -z "$INTERFACE_NAME" ]; then
-      echo "Interface name can be defined either as argument or environment variable 'INTERFACE_NAME'"
-      INTERFACE_NAME=$interface_name
-      echo "Interface name is set by default: '$INTERFACE_NAME'"
+    if [ -z "$TS_INTERFACE_NAME" ]; then
+      echo "Interface name can be defined either as argument or environment variable 'TS_INTERFACE_NAME'"
+      TS_INTERFACE_NAME=$default_interface_name
+      echo "Interface name is set by default: '$TS_INTERFACE_NAME'"
     else
-      echo "Interface name is: '$INTERFACE_NAME'"
+      echo "Interface name is: '$TS_INTERFACE_NAME'"
     fi
 else
-  INTERFACE_NAME=$1
-  echo "Interface name is: '$INTERFACE_NAME'"
+  TS_INTERFACE_NAME=$1
+  echo "Interface name is: '$TS_INTERFACE_NAME'"
 fi
 
+echo "Start flapping with the following papamptrams?
+  TS_INTERFACE_NAME:    $TS_INTERFACE_NAME
+  TS_NUMBER_OF_CYCLES:  $TS_NUMBER_OF_CYCLES
+  TS_SLEEP_TIME:        $TS_SLEEP_TIME
+  "
 read -p "Press enter to continue: "
 
-for (( c=1; c<=20; c++ )); do
-	echo "Interface $INTERFACE_NAME down"
-	ip link set $INTERFACE_NAME down
-	sleep 5s
-	echo "Interface $INTERFACE_NAME up"
-	ip link set $INTERFACE_NAME up
-	sleep 5s
+for (( c=1; c<=${TS_NUMBER_OF_CYCLES}; c++ )); do
+	echo "Interface $TS_INTERFACE_NAME down"
+	ip link set $TS_INTERFACE_NAME down
+	sleep $TS_SLEEP_TIME
+	echo "Interface $TS_INTERFACE_NAME up"
+	ip link set $TS_INTERFACE_NAME up
+	sleep $TS_SLEEP_TIME
 	date
 done
 echo "Finish"
