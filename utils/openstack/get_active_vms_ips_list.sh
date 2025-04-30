130117cb7d36927cf0cab4d1b51f23b94c37f123
ebochkov <ebochkov@itkey.com>
2025-04-30 10:14:09 +0300

    wip

--- utils/openstack/get_active_vms_ips_list.sh
+++ utils/openstack/get_active_vms_ips_list.sh
@@ -55,7 +55,8 @@ get_VMs_IPs () {
       VMs_IPs=\$(openstack server list $host_string $project_string |grep ACTIVE | grep -ohE \"$ip_pub_net_grep_string\")
     command to check connection:
       for ip in \$VMs_IPs;do ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=5 -q -i ~/test_scripts_keystack/key_test.pem ubuntu@\$ip echo ok; done
-  [DEBUG]: VMs_IPs: $VMs_IPs
+    VMs_IPs:
+    $VMs_IPs
   "
   # in openstack cli version 6.2 the --host key gives an empty output
   if [ -z "$VMs_IPs" ]; then
@@ -68,7 +69,8 @@ get_VMs_IPs () {
       VMs_IPs=\$(openstack server list $project_string --long | grep -E \"ACTIVE.*$HYPERVISOR_NAME\" | grep -ohE \"$ip_pub_net_grep_string\"
     command to check connection:
       for ip in \$VMs_IPs;do ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=5 -q -i ~/test_scripts_keystack/key_test.pem ubuntu@\$ip echo ok; done
-  [DEBUG]: VMs_IPs: $VMs_IPs
+    VMs_IPs:
+    $VMs_IPs
   "
 #      grep -E "ACTIVE.*$HYPERVISOR_NAME" |awk '{print \$12}')
     if [ -z "$VMs_IPs" ]; then
