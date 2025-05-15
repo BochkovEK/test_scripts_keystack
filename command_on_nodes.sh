6fd636fdc01b860c91b03c341de29c619715b984
ebochkov <ebochkov@itkey.com>
2025-05-15 16:56:53 +0300

    wip

--- command_on_nodes.sh
+++ command_on_nodes.sh
@@ -310,7 +310,7 @@ get_nodes_list () {
 get_nodes_list
 
 if [ "$DONT_CHECK_CONN" = false ]; then
-  echo "Check connection to $NODES_TYPE"
+  echo "Check: ping to $NODES_TYPE"
   for node in "${NODES[@]}"; do
     check_ping $node
   done
