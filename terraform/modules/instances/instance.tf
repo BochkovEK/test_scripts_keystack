1625fef107084648cd6564a3057b33bb855f3e2b
ebochkov <ebochkov@itkey.com>
2025-05-29 19:59:13 +0300

    wip

--- terraform/modules/instances/instance.tf
+++ terraform/modules/instances/instance.tf
@@ -66,7 +66,7 @@ resource "openstack_blockstorage_volume_v3" "additional_volume" {
 }
 
 # Подключение дополнительных томов
-resource "openstack_compute_volume_attach_v2" "volume_attachment" {
+resource "openstack_compute_volume_attach_v3" "volume_attachment" {
   for_each = openstack_blockstorage_volume_v3.additional_volume
 
   instance_id = openstack_compute_instance_v2.vm[each.value.name].id
