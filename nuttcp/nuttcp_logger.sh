38d8dc82cc876ce04b722f2bd4e41bf0aa5a22b1
ebochkov <ebochkov@itkey.com>
2025-06-04 16:55:33 +0300

    wip

--- nuttcp/nuttcp_logger.sh
+++ nuttcp/nuttcp_logger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Example usage: bash ./nuttcp_logger.sh 10.224.54.80 -u -i1 -T10
+# Example usage: bash ./nuttcp_logger.sh 10.224.54.80 -u -i1 -T10 -R 25m -f-drops
 
 #-u    UDP protocol
 #-R    Rate limit (m|M)bps
@@ -11,17 +11,21 @@ default_log_file="nuttcp_$(date '+%Y-%m-%d').log"
 [[ -z $LOG_FILE ]] && LOG_FILE=$default_log_file
 
 # Validate IP address format
-function validate_ip() {
-    local ip=$1
-    local stat=1
+#function validate_ip() {
+#    local ip=$1
+#    local stat=1
+#
+#    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+#        IFS='.' read -r -a octets <<< "$ip"
+#        [[ ${octets[0]} -le 255 && ${octets[1]} -le 255 && \
+#           ${octets[2]} -le 255 && ${octets[3]} -le 255 ]]
+#        stat=$?
+#    fi
+#    return $stat
+#}
 
-    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        IFS='.' read -r -a octets <<< "$ip"
-        [[ ${octets[0]} -le 255 && ${octets[1]} -le 255 && \
-           ${octets[2]} -le 255 && ${octets[3]} -le 255 ]]
-        stat=$?
-    fi
-    return $stat
+function validate_ip() {
+    grep -E -q '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$' <<< "$1"
 }
 
 # Check for server IP argument
