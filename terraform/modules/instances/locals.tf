acb9e41e77558d7237806347dd31c87e5789b9db
ebochkov <ebochkov@itkey.com>
2025-08-01 19:32:14 +0300

    wip

--- terraform/modules/instances/locals.tf
+++ terraform/modules/instances/locals.tf
@@ -11,7 +11,7 @@ locals {
         flavor_name                       = try(instance.flavor_name, var.default_flavor_name)
         keypair_name                      = try(instance.keypair_name, null) #var.default_key_pair_name)
         security_groups                   = try(instance.security_groups, null) #var.default_security_groups)
-        server_group_uuid                 = try(instance.server_group_uuid, null)
+#        server_group_uuid                 = try(instance.server_group_uuid, null)
 #        server_group                      = try(instance.server_group, null)
         az_hint                           = try(instance.az_hint, null)
         scheduler_hints                   = try(instance.scheduler_hints, null)
@@ -27,12 +27,12 @@ locals {
             ),
             # else use string or default empty string
             instance.user_data, var.default_user_data)
-       server_group_type = try(
-  instance.server_group != null ? "new" :
-  try(instance.server_group_uuid, null) != null ? "existing" :
-  null,
-  null
-)
+        server_group_type = try(
+          lookup(instance, "server_group", null) != null ? "new" :
+          lookup(instance, "server_group_uuid", null) != null ? "existing" :
+          null,
+          null
+        )
 #        server_group_type = try(instance.server_group != null ? "new" : instance.server_group_uuid != null ? "existing" : null, null)
 #        server_group_policy = try(instance.server_group.policy, null)
       }
