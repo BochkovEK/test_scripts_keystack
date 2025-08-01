653c6159e01228deefa6804c0561d5ea6c309a91
ebochkov <ebochkov@itkey.com>
2025-08-01 19:10:29 +0300

    wip

--- terraform/modules/instances/locals.tf
+++ terraform/modules/instances/locals.tf
@@ -39,11 +39,5 @@ locals {
   }
 }
 
-output "server_group_types" {
-  value = {
-    for inst in local.instances :
-    inst.name => inst.server_group_type
-  }
-  description = "Server group types for all instances"
-}
+
 
