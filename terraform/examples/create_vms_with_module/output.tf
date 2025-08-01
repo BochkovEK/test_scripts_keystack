5cab3b60d6c0677a1b7ea688fc2391198ced2839
ebochkov <ebochkov@itkey.com>
2025-08-01 19:08:27 +0300

    wip

--- terraform/examples/create_vms_with_module/output.tf
+++
@@ -1,7 +0,0 @@
-output "server_group_types" {
-  value = {
-    for inst in local.instances :
-    inst.name => inst.server_group_type
-  }
-  description = "Server group types for all instances"
-}
--- terraform/modules/instances/locals.tf
+++ terraform/modules/instances/locals.tf
@@ -39,5 +39,11 @@ locals {
   }
 }
 
-
+output "server_group_types" {
+  value = {
+    for inst in local.instances :
+    inst.name => inst.server_group_type
+  }
+  description = "Server group types for all instances"
+}
 
