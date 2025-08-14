c04731c310fc9b1715698e8011b68db116320320
ebochkov <ebochkov@itkey.com>
2025-08-14 19:47:13 +0300

    wip

--- check_docker_container_state_on_nodes.sh
+++ check_docker_container_state_on_nodes.sh
@@ -167,7 +167,8 @@ check_required_container () {
     container_exist="false"
     for container in $container_name_on_node; do
       [ "$TS_DEBUG" = true ] && echo -e "
-[DEBUG]: container_name: $container
+[DEBUG]:  container_name:         $container
+          container_name_on_node: $container_name_on_node
 "
       if [ "$container" = "$container_requaired" ]; then
         container_exist="true"
