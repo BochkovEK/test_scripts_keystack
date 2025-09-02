04c897064b52f102a4ac5540106acb32f624542d
ebochkov <ebochkov@itkey.com>
2025-09-03 00:30:03 +0300

    wip

--- baremetal_power_management.sh
+++ baremetal_power_management.sh
@@ -211,7 +211,7 @@ start_python_power_management_script () {
     else
       if [ -z $BMC_SUFFIX ]; then
         echo "Check bmc suffix by script $EDIT_HA_REGION_CONFIG..."
-        bmc_suffix=$(bash $script_dir/$EDIT_HA_REGION_CONFIG suffix| tail -n1)
+        bmc_suffix=$(bash $script_dir/$EDIT_HA_REGION_CONFIG -suffix| tail -n1)
         [[ -z $bmc_suffix ]] && { printf "%40s\n" "${red}variable bmc_suffix id empty${normal}"; exit 1; }
 #        bmc_suffix=$BMC_SUFFIX
       else
