ef65a99948a8513b12de5c60f34ae08b4dee26bb
ebochkov <ebochkov@itkey.com>
2025-09-02 19:47:41 +0300

    wip

--- utils/get_nodes_list.sh
+++ utils/get_nodes_list.sh
@@ -119,6 +119,7 @@ check_and_source_openrc_file () {
 # Function to find IP in hosts file
 find_in_hosts_file() {
     local hostname=$1
+    echo $TS_HOSTS_PATH
     [ -f "$TS_HOSTS_PATH" ] || { echo "file $TS_HOSTS_PATH does not exist"; return 1; }
     echo "bar"
     grep -w "$hostname" "$TS_HOSTS_PATH" | awk '{print $1}' | head -n1
