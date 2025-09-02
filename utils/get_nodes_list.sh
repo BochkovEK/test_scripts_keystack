b457e142dae9530c946376fbeac676c387f7d0c5
ebochkov <ebochkov@itkey.com>
2025-09-02 19:27:54 +0300

    wip

--- utils/get_nodes_list.sh
+++ utils/get_nodes_list.sh
@@ -127,7 +127,7 @@ find_in_hosts_file() {
 
 resolve_hostname_to_ips () {
     # Resolve hostnames to IPs
-    echo "foo"
+    echo "${NODES[*]}"
     for i in "${!NODES[@]}"; do
         local host="${NODES[$i]}"
         local ip=$(dig +short "$host" 2>/dev/null | head -n1)
