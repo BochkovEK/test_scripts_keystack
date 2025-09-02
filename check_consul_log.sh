4f932838a39eb476ab8b78dc25f3fdcbb31ab002
ebochkov <ebochkov@itkey.com>
2025-09-03 00:02:47 +0300

    wip

--- check_consul_log.sh
+++ check_consul_log.sh
@@ -171,9 +171,8 @@ get_nodes_list () {
   [ "$TS_DEBUG" = true ] && echo -e "
   [DEBUG]: \"\$NODES\": ${NODES[*]}
   "
-#  echo -e "
-#  NODES: ${NODES[*]}
-#  "
+  echo -e "${NODES[*]}"
+
   if [ -z "${NODES[*]}" ]; then
     echo -e "${red}Failed to determine node list - ERROR${normal}"
     exit 1
