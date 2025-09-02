ddceea03e95bd0f1b25038e519ce31a63dd7f965
ebochkov <ebochkov@itkey.com>
2025-09-02 20:12:32 +0300

    wip

--- utils/get_nodes_list.sh
+++ utils/get_nodes_list.sh
@@ -259,7 +259,8 @@ nodes_to_find: $nodes_to_find
 if [ -n "$NODES_NAME" ]; then
   echo "NODE_NAME: $NODES_NAME"
 #  NODES=("${NODES_NAME}")
-  NODES=($NODES_NAME)
+#  NODES=($NODES_NAME)
+  NODES=("${NODES_NAME[@]}")
   resolve_hostname_to_ips
   exit 0
 fi
