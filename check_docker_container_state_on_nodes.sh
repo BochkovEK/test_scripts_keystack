8f554fcef3b5c31c7b2ac46a403340aba0277584
ebochkov <ebochkov@itkey.com>
2025-09-03 15:39:09 +0300

    wip

--- check_docker_container_state_on_nodes.sh
+++ check_docker_container_state_on_nodes.sh
@@ -228,9 +228,11 @@ check_required_container () {
 
 get_nodes_list () {
   [ "$TS_DEBUG" = true ] && echo -e "get_nodes_list starting..."
-  if [ -z "${NODES[*]}" ]; then
-    nodes=$(bash $utils_dir/$get_nodes_list_script "-$1" "$2")
-  fi
+  nodes=$(bash $utils_dir/$get_nodes_list_script "-$1" "$2")
+#  if [ -z "${NODES[*]}" ]; then
+#    echo in get_nodes_list 1
+#    echo $nodes
+#  fi
 #  node=$(cat /etc/hosts | grep -m 1 -E ${nodes_pattern} | awk '{print $2}')
   [ "$TS_DEBUG" = true ] && echo -e "
   [DEBUG]:
