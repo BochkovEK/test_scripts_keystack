9ab37046d507739ea7c6074632fe8ce7ec4b2869
ebochkov <ebochkov@itkey.com>
2025-09-03 13:02:02 +0300

    wip

--- terraform/examples/image_from_file/main.tf
+++ terraform/examples/image_from_file/main.tf
@@ -1,14 +1,14 @@
 resource "openstack_images_image_v2" "ubuntu_image" {
-  name             = "Ubuntu-22.04-Custom"
+  name             = "ubuntu-22.04-server-cloudimg-amd64.img"
 #  image_source_url = "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
   local_file_path  = "/root/.terraform/image_cache/1d560741ef4720a143a16bc7db440c7b.img"
   container_format = "bare"
-  disk_format      = "raw" #qcow2
+  disk_format      = "qcow2" #raw
   visibility       = "public" #"private" # или "public", "shared", "community"
 
   min_disk_gb = 5
   min_ram_mb  = 1024
-  tags        = ["ubuntu", "22.04", "custom"]
+  tags        = ["ubuntu", "22.04", "tf"]
 
   protected = false
 
