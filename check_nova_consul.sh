898c82aefd43fb9e486e6b75ed544d87c5b49d6b
ebochkov <ebochkov@itkey.com>
2025-09-05 00:05:50 +0300

    wip

--- check_nova_consul.sh
+++ check_nova_consul.sh
@@ -302,6 +302,7 @@ check_disabled_computes() {
                     export CHECK_OPENSTACK="false"
                     export COMP_NODE_NAME="$cmpt"
                     export CHECK_AFTER="false"
+                    export SSH_USER=$SSH_USER
                     [ "$TS_DEBUG" = true ] && echo -e "try check script file $openstack_utils/$try_to_rise_compute_node_script"
                     if [ -f "$openstack_utils/$try_to_rise_compute_node_script" ]; then
                         [ "$TS_DEBUG" = true ] && echo -e "$try_to_rise_compute_node_script exists - ok"
