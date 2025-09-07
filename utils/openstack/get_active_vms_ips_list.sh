59dfcff4a19b42150cab82bf82d7ae80bdaef968
ebochkov <ebochkov@itkey.com>
2025-09-07 14:55:56 +0300

    wip

--- utils/openstack/get_active_vms_ips_list.sh
+++ utils/openstack/get_active_vms_ips_list.sh
@@ -143,14 +143,14 @@ get_vms_info() {
         Command: openstack server list $project_string $host_string --long -f value -c Name -c Status -c Networks 2>/dev/null | \
             grep -E \"$vm_name_pattern\"
     "
-        vm_list=$(openstack server list "$project_string" "$host_string" --long -f value -c Name -c Status -c Networks 2>/dev/null | \
+        vm_list=$(openstack server list "$project_string" $host_string --long -f value -c Name -c Status -c Networks 2>/dev/null | \
             grep -E "${vm_name_pattern}")
     else
         [ "$TS_DEBUG" = "true" ] && echo -e "
     [DEBUG]:
         Command: openstack server list $project_string $host_string --long -f value -c Name -c Status -c Networks 2>/dev/null
     "
-        vm_list=$(openstack server list "$project_string" "$host_string" --long -f value -c Name -c Status -c Networks 2>/dev/null)
+        vm_list=$(openstack server list "$project_string" $host_string --long -f value -c Name -c Status -c Networks 2>/dev/null)
     fi
 
     [ "$TS_DEBUG" = "true" ] && echo -e "
