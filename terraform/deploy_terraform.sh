a8674c81efd2e6d773e77e53c59fef6546bf5b8d
ebochkov <ebochkov@itkey.com>
2025-09-16 15:15:34 +0300

    wip

--- terraform/deploy_terraform.sh
+++ terraform/deploy_terraform.sh
@@ -82,7 +82,7 @@ install_terraform () {
       curl -o $script_dir/terraform $repo/$terraform_binary_name
     fi
     chmod 777 $script_dir/terraform
-#    mv $script_dir/$terraform_binary_name /usr/local/bin/terraform
+    mv $script_dir/$terraform_binary_name /usr/local/bin/terraform
 
     cat <<-EOF > ~/.terraformrc
 provider_installation {
