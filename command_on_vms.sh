ee311810b97ee2eee67476ae06087a460cd3232e
ebochkov <ebochkov@itkey.com>
2025-09-16 15:48:30 +0300

    wip

--- command_on_vms.sh
+++ command_on_vms.sh
@@ -181,6 +181,7 @@ get_vms_ips() {
     # Execute the command and capture output
     VMS=$(bash "$openstack_utils/$get_vms_list_script" $command_args 2>&1)
     [ "$TS_DEBUG" = "true" ] && echo -e "[DEBUG] $VMS"
+    [ "$TS_DEBUG" = "true" ] && echo -e "[DEBUG] exit_code $?"
 
     local exit_code=$?
     if [ $exit_code -ne 0 ]; then
