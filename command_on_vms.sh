ac98ade69c05fbad4ed1e75874020c3a778e0656
ebochkov <ebochkov@itkey.com>
2025-09-18 20:29:39 +0300

    wip

--- command_on_vms.sh
+++ command_on_vms.sh
@@ -181,6 +181,7 @@ get_vms_ips() {
 #    echo -e "[DEBUG] Command: bash \"$openstack_utils/$get_vms_list_script\" $command_args"
 
     # Execute the command and capture output
+    [ "$TS_DEBUG" = "true" ] && echo -e "[DEBUG] VMS command: bash \"$openstack_utils/$get_vms_list_script" $command_args"
     VMS=$(bash "$openstack_utils/$get_vms_list_script" $command_args)
 #     2>&1)
     [ "$TS_DEBUG" = "true" ] && echo -e "[DEBUG] command_args: $command_args"
