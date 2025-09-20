6f8a2c042855d2ea01607a858c0ff8666c7df8a5
ebochkov <ebochkov@itkey.com>
2025-09-20 16:54:50 +0300

    wip

--- check_ips_list.sh
+++ check_ips_list.sh
@@ -1,33 +1,70 @@
 #!/bin/bash
 
-
-#Colors
-#Color       #define       Value       RGB
-#black     COLOR_BLACK       0     0, 0, 0
-#red       COLOR_RED         1     max,0,0
-#green     COLOR_GREEN       2     0,max,0
-#yellow    COLOR_YELLOW      3     max,max,0
-#blue      COLOR_BLUE        4     0,0,max
-#magenta   COLOR_MAGENTA     5     max,0,max
-#cyan      COLOR_CYAN        6     0,max,max
-#white     COLOR_WHITE       7     max,max,max
+# Colors
 normal=$(tput sgr0)
 yellow=$(tput setaf 3)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
 
-BASE_IP=10.224.140
-START=97
-END=126
+# Function to show usage
+usage() {
+    echo "Usage: $0 \"<start_ip> <end_ip>\""
+    echo "Example: $0 \"10.224.140.97 10.224.140.126\""
+    exit 1
+}
 
-## save $START, just in case if we need it later ##
-i=$START
-while [[ $i -le $END ]]; do
-	echo "$BASE_IP.$i"
-	ping -c 2 $BASE_IP.$i &> /dev/null
-	if ping -c 2 $IP &> /dev/null; then
-    printf "%40s\n" "${yellow}There is a connection with $BASE_IP.$i - success${normal}"
-#    [ "$ONLY_PING" == "false" ] && { ssh -t -o StrictHostKeyChecking=no -i $script_dir/$KEY_NAME $VM_USER@$IP "$COMMAND_STR"; }
-  else
-    printf "%40s\n" "No connection with $IP - error!"
-  fi
-  ((i = i + 1))
+# Check if argument is provided
+if [ $# -ne 1 ]; then
+    echo -e "${red}Error: Missing required argument${normal}"
+    usage
+fi
+
+# Parse the input argument
+IP_RANGE=$1
+
+# Split the input into start and end IPs
+START_IP=$(echo "$IP_RANGE" | awk '{print $1}')
+END_IP=$(echo "$IP_RANGE" | awk '{print $2}')
+
+# Validate IP addresses
+if ! echo "$START_IP" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' || \
+   ! echo "$END_IP" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'; then
+    echo -e "${red}Error: Invalid IP format${normal}"
+    usage
+fi
+
+# Extract network part and host parts
+NETWORK_PART=$(echo "$START_IP" | cut -d. -f1-3)
+START_HOST=$(echo "$START_IP" | cut -d. -f4)
+END_HOST=$(echo "$END_IP" | cut -d. -f4)
+
+# Validate that both IPs are in the same network
+if [ "$NETWORK_PART" != "$(echo "$END_IP" | cut -d. -f1-3)" ]; then
+    echo -e "${red}Error: IP addresses must be in the same network${normal}"
+    usage
+fi
+
+# Validate host ranges
+if [ "$START_HOST" -gt "$END_HOST" ]; then
+    echo -e "${red}Error: Start IP must be less than or equal to End IP${normal}"
+    usage
+fi
+
+echo -e "${green}Pinging IP range: $START_IP to $END_IP${normal}"
+echo ""
+
+i=$START_HOST
+while [[ $i -le $END_HOST ]]; do
+    IP="$NETWORK_PART.$i"
+    echo "Testing $IP"
+
+    if ping -c 2 -W 1 "$IP" &> /dev/null; then
+        printf "%40s\n" "${yellow}There is a connection with $IP - success${normal}"
+    else
+        printf "%40s\n" "${red}No connection with $IP - error!${normal}"
+    fi
+    ((i = i + 1))
 done
+
+echo ""
+echo -e "${green}Scan completed!${normal}"
