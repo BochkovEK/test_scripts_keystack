8eb07d6dafe2bc867ddc3f230f83ea9992e5ffb6
ebochkov <ebochkov@itkey.com>
2025-09-26 16:37:45 +0300

    wip

--- check_consul_log.sh
+++ check_consul_log.sh
@@ -218,14 +218,14 @@ find_consul_leader() {
             if [ "$mode" = "mtls" ];then
                 echo "
                 leader=\$(ssh -t -o StrictHostKeyChecking=no \"$SSH_USER@$node_ip\" \
-                    \"sudo $CONTAINER_ENGINE consul operator raft list-peers
+                    \"sudo $CONTAINER_ENGINE exec consul consul consul operator raft list-peers
                      -http-addr=https://$node_ip:8501 -ca-file $https_ssl_verify
                      -client-cert $client_cert
                      -client-key $client_key 2>/dev/null\" | \
                     grep leader | awk '{print \$1}')
                 "
                 leader=$(ssh -t -o StrictHostKeyChecking=no "$SSH_USER@$node_ip" \
-                    "sudo $CONTAINER_ENGINE consul operator raft list-peers
+                    "sudo $CONTAINER_ENGINE exec consul consul operator raft list-peers
                      -http-addr=https://$node_ip:8501 -ca-file $https_ssl_verify
                      -client-cert $client_cert
                      -client-key $client_key 2>/dev/null" | \
