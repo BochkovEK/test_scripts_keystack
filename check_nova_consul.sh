e27788ac2023822701ad92e2b3c9b0168dc911bf
ebochkov <ebochkov@itkey.com>
2025-09-27 17:04:10 +0300

    wip

--- check_nova_consul.sh
+++ check_nova_consul.sh
@@ -448,9 +448,9 @@ check_consul_members() {
         if [ "$mode" = "mtls" ];then
             [ "$TS_DEBUG" = true ] && echo -e "
     members_list=\$(ssh -t -o StrictHostKeyChecking=no \"$SSH_USER@$node_ip\" \
-        \"sudo $CONTAINER_ENGINE exec -it consul consul members list
-        -http-addr=https://$node_ip:8501 -ca-file $https_ssl_verify
-        -client-cert $client_cert
+        \"sudo $CONTAINER_ENGINE exec -it consul consul members list \
+        -http-addr=https://$node_ip:8501 -ca-file $https_ssl_verify \
+        -client-cert $client_cert \
         -client-key $client_key 2>/dev/null\")
                 "
             members_list=$(ssh -t -o StrictHostKeyChecking=no "$SSH_USER@$node_ip" \
