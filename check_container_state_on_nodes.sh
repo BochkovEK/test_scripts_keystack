f32e86e245cfad70daf7672bb7daff1de9955f14
ebochkov <ebochkov@itkey.com>
2025-09-30 14:58:07 +0300

    wip

--- check_container_state_on_nodes.sh
+++ check_container_state_on_nodes.sh
@@ -188,7 +188,8 @@ check_required_containers() {
         if [ "$container_exists" = "false" ]; then
             local container_required_note="${container_required#*:}"
             echo "$container_required_note"
-            if [[ "$container_required" == "*:*" ]] && [ -n "$container_required_note" ]; then
+#            [[ "$container_required" == "*:*" ]] &&
+            if [ -n "$container_required_note" ]; then
                 echo "$container_required_note"
                 echo -e "${yellow}Container $container_required not running - Warning${normal}"
             else
