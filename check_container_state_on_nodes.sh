ad2ab63ab81213064283744c718174f45ed3636c
ebochkov <ebochkov@itkey.com>
2025-10-13 14:18:45 +0300

    wip

--- check_container_state_on_nodes.sh
+++ check_container_state_on_nodes.sh
@@ -314,7 +314,7 @@ main() {
         exit 1
     fi
 
-    echo -e "${green}Using SSH user: $SSH_USER${normal}"
+    echo -e "Using SSH user: $SSH_USER"
 
     # Get nodes list
     if [ -n "$NODES_NAME" ]; then
