729faaea2557ea7c74ecdf1f0fdff0759a8b1a4d
ebochkov <ebochkov@itkey.com>
2025-10-28 20:05:41 +0300

    wip

--- create_vms.sh
+++ create_vms.sh
@@ -819,7 +819,7 @@ check_image() {
 # Determine flavor name for search and creation
 get_flavor_name() {
     local base_flavor="${1:-$FLAVOR}"
-    local project="$2:$PROJECT"
+    local project="${2:-$PROJECT}"
 
     if [[ "$base_flavor" =~ ^[0-9]+c-[0-9]+r$ ]]; then
         echo "${base_flavor}_${project}"
