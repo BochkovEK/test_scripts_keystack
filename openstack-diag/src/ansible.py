71cc2265cc50cbd5144616ea5c4592d3cd595ffd
ebochkov <ebochkov@itkey.com>
2025-11-05 19:45:07 +0300

    wip

--- openstack-diag/src/ansible.py
+++ openstack-diag/src/ansible.py
@@ -61,6 +61,7 @@ class Ansible:
                 playbook=str(playbook_path),
                 inventory=str(self.inventory_path),
                 private_data_dir=str(self.ansible_path),
+                quiet=True,  # hides output under the hood
                 envvars={
                     'ANSIBLE_CONFIG': str(self.ansible_cfg_path)
                 }
