ab6302598de3ccb4fcad5ca7b69d6a7d5954fade
ebochkov <ebochkov@itkey.com>
2025-11-09 17:58:07 +0300

    wip

--- openstack-pulse/test_mariadb_requests.py
+++ openstack-pulse/test_mariadb_requests.py
@@ -30,7 +30,7 @@ def test_mariadb_connection():
         #     cursorclass=pymysql.cursors.DictCursor
         # )
         connection = mysql.connector.connect(
-            host=host,
+            host="foo",
             user='username',
             password='password',
             port=3306,
