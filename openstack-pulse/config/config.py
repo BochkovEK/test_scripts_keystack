9ea8334d7fba3822f9295b2245ec85ddc1c3ff12
ebochkov <ebochkov@itkey.com>
2025-11-09 17:15:02 +0300

    wip

--- openstack-pulse/config/config.py
+++ openstack-pulse/config/config.py
@@ -10,7 +10,7 @@ class ServiceType(Enum):
     """Service types for authentication"""
     OPENSTACK = "openstack"
     RABBITMQ = "rabbitmq"
-    MARIADB = "mariadb"
+    MARIADB = "galera"
 
 
 class DotDict:
