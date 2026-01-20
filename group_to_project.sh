13e47ef469360b2595c1ce3521c59abfad8568c6
ebochkov <ebochkov@itkey.com>
2026-01-20 22:54:52 +0300

    wip

--- group_to_project.sh
+++ group_to_project.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-project='demo'
-user_domain='itkey'
-domain=$user_domain
+#project='demo'
+#user_domain='itkey'
+#domain=$user_domain
 
 declare -A group_role_map
 group_role_map=(
@@ -18,6 +18,14 @@ function run() {
         (( DEBUG )) && printf 'Running: %s\n' "$*" >&2
         (( DRY_RUN )) || "$@"
 }
+
+read -r -p "Projcet name [demo]: " project
+project=${project:-demo}
+
+read -r -p "Domain name [itkey]: " domain
+domain=${domain:-itkey}
+
+user_domain="$domain"
 
 declare -A group_id_map
 while mapfile -t -n 2 ary && (( ${#ary[@]} )); do
