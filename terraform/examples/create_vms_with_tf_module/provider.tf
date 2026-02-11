a82fb2341301caa01322cc3856859462003e6d94
ebochkov <ebochkov@itkey.com>
2026-02-11 08:25:09 +0300

    wip

--- terraform/examples/create_vms_with_tf_module/provider.tf
+++ terraform/examples/create_vms_with_tf_module/provider.tf
@@ -1,11 +1,23 @@
+#terraform {
+#required_version = ">= 0.14.0"
+#  required_providers {
+#    openstack = {
+#      source  = "terraform-provider-openstack/openstack"
+#      version = "~> 1.48.0"
+#    }
+#  }
+#}
 terraform {
-required_version = ">= 0.14.0"
   required_providers {
+    null = {
+      source = "hashicorp/null"
+    }
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.48.0"
+      version = ">=1.42.0"
     }
   }
+  required_version = ">= 0.13"
 }
 
 # authentication by clouds.yml file or ENVs
--- terraform/modules/instances/version.tf
+++ terraform/modules/instances/version.tf
@@ -1,9 +1,21 @@
+#terraform {
+#required_version = ">= 0.14.0"
+#  required_providers {
+#    openstack = {
+#      source  = "terraform-provider-openstack/openstack"
+#      version = "~> 1.48.0"
+#    }
+#  }
+#}
 terraform {
-required_version = ">= 0.14.0"
   required_providers {
+    null = {
+      source = "hashicorp/null"
+    }
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.48.0"
+      version = ">=1.42.0"
     }
   }
+  required_version = ">= 0.13"
 }
