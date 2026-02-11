c10c9110105dc8f8f89b9b98a8600fbdab192ac0
ebochkov <ebochkov@itkey.com>
2026-02-11 09:05:01 +0300

    wip

--- terraform/modules/instances/version.tf
+++ terraform/modules/instances/version.tf
@@ -1,12 +1,8 @@
 terraform {
   required_providers {
-    null = {
-      source = "hashicorp/null"
-    }
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = ">=1.42.0"
+      version = ">= 1.42.0"
     }
   }
-  required_version = ">= 0.13"
 }
